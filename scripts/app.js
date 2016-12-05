564b4bc9fe27b966affd043bae7857f287e0f342
Paulina Twarog <paulina.twarog@gmail.com>
2016-12-05 12:13:18 +0100

    add welcome page

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,33 @@
 $(document).ready(function() {
     var elem = $("#MyDiv2");
+    
+    var x = 0;
+    var y = 60;
+    
     elem.on("click", function(){
-        AnimateRotate(0, 60); //off to fast
-//        AnimateRotate(60, 120); //fast to daily
-//        AnimateRotate(120, 180); //daily to custom
-//        AnimateRotate(180, 240); //custom to hand
-//        AnimateRotate(240, 300); //hand to cotton
-//        AnimateRotate(300, 360); //cotton to off
+        AnimateRotate(x, y);
+        x += 60;
+        y += 60;
+    });
+    
+    
+    
+    var btnClose = document.createElement("button");
+    var imgSnoopy = document.querySelector(".container_open");
+    btnClose.classList.add("close");
+    btnClose.innerText = "Close";
+    imgSnoopy.appendChild(btnClose);
+
+
+// guzik zaglady
+    btnClose.addEventListener("click", function(event){
+      this.parentElement.remove(imgSnoopy);
     });
     
 });
 
-function AnimateRotate(from, to){
-    var elem = $("#MyDiv2");
+function AnimateRotate(from, to, i){
+    var element = $("#MyDiv2");
     
     $({deg: from}).animate({deg: to}, {
         duration: 2000,
@@ -20,15 +35,34 @@ function AnimateRotate(from, to){
             // in the step-callback (that is fired each step of the animation),
             // you can use the `now` paramter which contains the current
             // animation-position (`0` up to `angle`)
-            elem.css({
+            element.css({
                 transform: 'rotate(' + now + 'deg)'
-            });
-            
-        }
+            });      
+        }    
     });
-    
 }
 
+//var washingMode = function(name, time) {
+//  this.name = name;
+//  this.time = time;
+//};
+//
+//var array = [
+//    {
+//      name: Fast,
+//      time: 10,
+//      steps: [ 
+//        water heating,
+//        motor control,
+//        rinsing,
+//        centrifugation,
+//        drying
+//      ]  
+//    }
+//    
+//];
 
-
-
+//
+//function fastMode(){
+//    
+//}; 
