efcbb38c700d09d8f26504aca1c7d2cecd3bdc48
Paulina Twarog <paulina.twarog@gmail.com>
2016-12-07 09:26:13 +0100

    add mode functions

--- scripts/app.js
+++ scripts/app.js
@@ -1,19 +1,20 @@
 $(document).ready(function() {
     
     
-    //show welcome site
-    
-    var btnClose = document.createElement("button");
-    var imgSnoopy = document.querySelector(".container_open");
-    btnClose.classList.add("close");
-    btnClose.innerText = "Close";
-    imgSnoopy.appendChild(btnClose);
-
-
-    //wreck buttton to hide welcome site
-    btnClose.addEventListener("click", function(event){
-      this.parentElement.remove(imgSnoopy);
-    });
+//    //show welcome site
+//    
+//    var btnClose = document.createElement("button");
+//    var imgSnoopy = document.querySelector(".container_open");
+//    btnClose.classList.add("close");
+//    btnClose.innerText = "Close";
+//    imgSnoopy.appendChild(btnClose);
+//
+//
+//    //wreck buttton to hide welcome site
+//    btnClose.addEventListener("click", function(event){
+//      this.parentElement.remove(imgSnoopy);
+//    });
+//    
     
     
     
@@ -36,7 +37,8 @@ $(document).ready(function() {
     };
     
     
-       //create constructor for objects with names, duration and steps of washing
+    
+    //create constructor for objects with names, duration and steps of washing
     
     var parentsteps = document.querySelector(".icon").children;
 
@@ -44,30 +46,30 @@ $(document).ready(function() {
         this.name = name;
         this.time = time;
         this.steps = [
-            parentsteps[0].innerText, 
-            parentsteps[1].innerText, 
-            parentsteps[2].innerText, 
-            parentsteps[3].innerText, 
-            parentsteps[4].innerText, 
-            parentsteps[5].innerText, 
-            parentsteps[6].innerText
+            parentsteps[0].firstElementChild, 
+            parentsteps[1].firstElementChild,
+            parentsteps[2].firstElementChild, 
+            parentsteps[3].firstElementChild, 
+            parentsteps[4].firstElementChild, 
+            parentsteps[5].firstElementChild, 
+            parentsteps[6].firstElementChild
         ];
-        this.quick_time = [4, t2, t3, t4, t5, t6, 5];
+        this.quickTime = [3000, t2, t3, t4, t5, t6, 5000];
     };
 
     
+
     //create objects of laundry
     
-    var fastMode = new WashingMode("fast", 10, 2, 2, 2, 2, 2);
-    var dailyMode = new WashingMode("daily", 20, 4, 4, 4, 4, 4);
+    var fastMode = new WashingMode("fast", 10, 2000, 2000, 2000, 2000, 2000);
+    var dailyMode = new WashingMode("daily", 20, 4000, 4000, 4000, 4000, 4000);
     var customMode = new WashingMode("custom", 0, 0, 0, 0, 0, 0);
-    var handMode = new WashingMode("hand", 14, 3, 4, 2, 3, 2);
-    var cottonMode = new WashingMode("cotton", 22, 4, 6, 2, 5, 5);
-
-//    console.log(customMode.quick_time[5]);
-
+    var handMode = new WashingMode("hand", 14, 3000, 4000, 2000, 3000, 2000);
+    var cottonMode = new WashingMode("cotton", 22, 4000, 6000, 2000, 5000, 5000);
     
+  
     
+ 
     //animate the washing knob
     //and show name and duration of the mode on the screen
     
@@ -82,54 +84,175 @@ $(document).ready(function() {
         
             var name = document.getElementById("name");
             var duration = document.getElementById("duration");
+            var on = document.getElementById("on").firstElementChild; 
         
             var thisName = " ";
             var thisTime = " ";
+         
     
         if(x === 0 && y === 60){
              thisName = fastMode.name;
              thisTime = fastMode.time;
+             on.classList.add("change");
         }
         else if(x === 60 && y === 120){
             thisName = dailyMode.name;
             thisTime = dailyMode.time;
-
+            on.classList.add("change");
         }
         else if(x === 120 && y === 180){
             thisName = customMode.name;
             thisTime = customMode.time;
+            on.classList.add("change");
         }  
         else if(x === 180 && y === 240){
             thisName = handMode.name;
             thisTime = handMode.time;
+            on.classList.add("change");
         }
         else if(x === 240 && y === 300){
             thisName = cottonMode.name;
             thisTime = cottonMode.time;
+            on.classList.add("change");
         }
         else if(x === 300 && y === 360){
             thisName = "off";
             thisTime = " ";
-        }
-        
+            on.classList.remove("change");
+            }
+ 
         name.innerText = thisName.toUpperCase();
         duration.innerText = thisTime;
-        
+
         x += 60;
-        y += 60; 
+        y += 60;
+        
     });
-    
-    
- 
     
     
     
     //find buttons start, stop, drying, edit
     
-    var startBtn = document.getElementById("start");
-    var stopBtn = document.getElementById("stop");
-    var dryBtn = document.getElementById("drying");
-    var editBtn = document.querySelector(".edit_btn");
+    startBtn = document.getElementById("start");
+    stopBtn = document.getElementById("stop");
+    dryBtn = document.getElementById("drying");
+    editBtn = document.getElementById("edit");
+    
+    
+    
+    //check whether a btn was clicked
+ 
+    var btns = document.querySelectorAll("button");
+    
+    
+    for(var i=0; i<btns.length; ++i) {
+        
+        btns[i].addEventListener("click", function(){
+            
+        var name = this.id;
+        var clicked = parseInt(this.dataset.clicked);
+        var nameMode = document.getElementById("name").innerText.toLowerCase();
+            
+        var stepBtns = Array.from(document.querySelectorAll(".step_btn"));
+          
+            switch(name){
+                case "start": {
+                    
+                    console.log("rozpoczynam pranie");
+                    
+                    if(nameMode === "fast"){
+                        wHeat(fastMode);
+                    }
+                    
+                    else if(nameMode === "daily"){
+                        wHeat(dailyMode);
+                    }
+                    
+                     else if(nameMode === "custom"){
+                        wHeat(customMode);
+                    }
+                    
+                     else if(nameMode === "hand"){
+                        wHeat(handMode);
+                    }
+                    
+                     else if(nameMode === "cotton"){
+                        wHeat(cottonMode);
+                    }
+                    
+                    break;
+                    
+                }
+                    
+                case "stop":
+                    console.log("koncze pranie");
+                    
+                    stepBtns.forEach(function(elements){
+                        elements.firstElementChild.classList.remove("change");
+                    });
+                    
+                    stepBtns[6].firstElementChild.classList.add("change");
+                    
+                    break;
+            }
+
+      });
+          
+    }
+    
+    
+    
+    //functions of different washing modes
+    
+    function wHeat(mode) {
+        mode.steps[6].classList.remove("change");
+        mode.steps[0].classList.remove("change");
+        mode.steps[1].classList.add("change");
+        
+        setTimeout(function(){
+            mControl(mode);
+        }, mode.quickTime[1]);
+    }
+    
+    function mControl(mode){
+        mode.steps[1].classList.remove("change");
+        mode.steps[2].classList.add("change"); 
+        setTimeout(function(){
+            rins(mode);
+        }, mode.quickTime[2]);
+    }
+    
+    function rins(mode){
+        mode.steps[2].classList.remove("change");
+        mode.steps[3].classList.add("change");
+        setTimeout(function(){
+            centrif(mode)
+        }, mode.quickTime[3]);      
+    }
+    
+    function centrif(mode){
+        mode.steps[3].classList.remove("change");
+        mode.steps[4].classList.add("change");
+        setTimeout(function(){
+            drying(mode)
+        }, mode.quickTime[4]);   
+    }
+    
+    function drying(mode){
+        mode.steps[4].classList.remove("change");
+        mode.steps[5].classList.add("change");
+        setTimeout(function(){
+            open(mode)
+        }, mode.quickTime[5]);
+    }
+    
+    function open(mode){
+        mode.steps[5].classList.remove("change");
+        mode.steps[6].classList.add("change");
+        
+    }
+        
+
     
     
     
